a4f04cf78b551ceeeea27826953c671fc793ca71
Orca <orca.zhang@yahoo.com>
2021-12-21 19:38:21 +0800

    :fish: update for BindToExistPool

--- stats/stats.go
+++ stats/stats.go
@@ -18,12 +18,11 @@ type StatsNode struct {
 // Bind - to stats a cache
 // `pool` can be used to classify instances that store same items
 func Bind(pool string, caches ...*cache.Cache) error {
-	v := &StatsNode{}
-	m.LoadOrStore(pool, v)
+	v, _ := m.LoadOrStore(pool, &StatsNode{})
 	for _, c := range caches {
 		c.Inspect(func(action int, _ string, ok int) {
 			// very, very, very low-cost for stats
-			atomic.AddUint64((*uint64)(unsafe.Pointer(uintptr(unsafe.Pointer(v))+uintptr((action-1)*2+ok+1)*unsafe.Sizeof(&ok))), 1)
+			atomic.AddUint64((*uint64)(unsafe.Pointer(uintptr(unsafe.Pointer(v.(*StatsNode)))+uintptr((action-1)*2+ok+1)*unsafe.Sizeof(&ok))), 1)
 		})
 	}
 	return nil
--- stats/stats_test.go
+++ stats/stats_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/orca-zhang/cache"
 )
 
-func Test_LRU2Cache(t *testing.T) {
+func TestLRU2Cache(t *testing.T) {
 	lc := cache.NewLRUCache(1, 3, 1*time.Second).LRU2(1)
 	Bind("lc", lc)
 	lc.Put("1", "1")
@@ -44,10 +44,10 @@ func Test_LRU2Cache(t *testing.T) {
 	})
 }
 
-var lc = cache.NewLRUCache(4, 1, 2*time.Second)
-var _ = Bind("lc", lc)
+var lc = cache.NewLRUCache(4, 1, 2*time.Second).LRU2(1)
+var _ = Bind("aaaa", lc)
 
-func Test_concurrent(t *testing.T) {
+func TestConcurrent(t *testing.T) {
 	var wg sync.WaitGroup
 	for index := 0; index < 1000000; index++ {
 		wg.Add(3)
@@ -65,4 +65,45 @@ func Test_concurrent(t *testing.T) {
 		}()
 	}
 	wg.Wait()
+	Stats().Range(func(k, v interface{}) bool {
+		fmt.Printf("stats: %s %+v\n", k, v)
+		return true
+	})
+}
+
+func TestBindToExistPool(t *testing.T) {
+	lcOld := cache.NewLRUCache(1, 3, 1*time.Second).LRU2(1)
+	Bind("lc", lcOld)
+	lc := cache.NewLRUCache(1, 3, 1*time.Second).LRU2(1)
+	Bind("lc", lc)
+	lc.Put("1", "1")
+	lc.Put("2", "2")
+	lc.Put("3", "3")
+	lc.Get("2") // l0 -> l1
+	lc.Get("3") // l0 -> l1
+	if _, ok := lc.Get("2"); ok {
+		t.Error("case 4 failed")
+	}
+	lc.Put("4", "4")
+	lc.Put("5", "5")
+	if _, ok := lc.Get("1"); !ok {
+		t.Error("case 4 failed")
+	}
+	lc.Put("6", "6")
+	lc.Put("7", "7")
+	if _, ok := lc.Get("4"); ok {
+		t.Error("case 4 failed")
+	}
+	lc.Del("7")
+	lc.Del("8")
+	lc.Put("1", "1")
+	lc.Put("1", "2")
+	lc.Del("1")
+	if _, ok := lc.Get("1"); ok {
+		t.Error("case 4 failed")
+	}
+	Stats().Range(func(k, v interface{}) bool {
+		fmt.Printf("stats: %s %+v\n", k, v)
+		return true
+	})
 }
